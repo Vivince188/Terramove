0db505d859dba8b294ba28406a6e5ee8d225a152
rj2396 <48699369+rj2396@users.noreply.github.com>
2026-01-28 15:49:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIioy1klCmRjZrROpLnjqRB-L2EEckUiM",
+  authDomain: "terramove-75646.firebaseapp.com",
+  databaseURL: "https://terramove-75646-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "terramove-75646",
+  storageBucket: "terramove-75646.firebasestorage.app",
+  messagingSenderId: "502743537596",
+  appId: "1:502743537596:web:da5487f58873630fa1fdb9"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const sensorRef = ref(database, "sensor");
+
+onValue(sensorRef, (snapshot) => {
+  const data = snapshot.val();
+  if (data) {
+    document.getElementById("tilt").innerText = data.tilt ?? "--";
+    document.getElementById("moisture").innerText = data.moisture ?? "--";
+  }
+});
